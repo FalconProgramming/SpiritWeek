3ff141b24f99ffc177a290abdfa931442f0f0cec
Edmond1 <edmonyx@yahoo.com>
2014-11-14 21:16:32 -0500

    More Test Stuff
    Don't worry about it

--- Assets/TestTextBox.js
+++ Assets/TestTextBox.js
@@ -1,7 +1,17 @@
 ﻿#pragma strict
 
+var playerObject = GameObject.Find("Hero");
+var playerPos:Vector3 = playerObject.transform.position;
+
+function Update () {
+
+}
+
+
 function OnGUI () {
-    if (GUI.Button (Rect (10,70, 100, 20), "Mr. Codega")) {
-        print ("Welcome to the People's Republic of Codega.");
+    if (GUI.Button (Rect (10,70, 100, 20), "Player's position")) {
+        print ("text");
     }
+    GUI.Box (Rect (10,10,150,20), "This is a text box.");
+
 }
