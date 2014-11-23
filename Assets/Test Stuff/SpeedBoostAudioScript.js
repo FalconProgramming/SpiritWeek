b25a46d03a913551ed7c00f0fe347515648eafcb
SssnakeTheJake <jmstanley2007@aol.com>
2014-11-23 18:50:08 -0500

    Fixed infinite speed bug
    When you collide with the speed boost, it sends the item to 100, 100.

--- Assets/Test Stuff/SpeedBoostAudioScript.js
+++ Assets/Test Stuff/SpeedBoostAudioScript.js
@@ -2,14 +2,15 @@
 
 var Player : Rigidbody2D;
 var soundFile : AudioClip;
+var speedBoostAudioScript: Rigidbody2D;
 
 function OnTriggerEnter2D (collision : Collider2D) {
 	if (collision.tag == "Player") {
 		Movement.speed = Movement.speed + 5f;
 		audio.clip = soundFile;
 		audio.Play();
+		speedBoostAudioScript.position = Vector2(100, 100);
 		yield WaitForSeconds (audio.clip.length);
 		Movement.speed = Movement.speed - 5f;
-		Destroy(gameObject);
 }
 }
