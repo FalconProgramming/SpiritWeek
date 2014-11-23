905e95150dcc67c206095c2285d2207b60f65a80
Spinark167 <jhandrews1@gmail.com>
2014-11-23 18:59:12 -0500

    properly added sound
    sound was added properly

---
+++ Assets/BackAndForthMovementInTheY.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var speed : float = 5;
+var timeset : float;
+var timer : float = timeset;
+
+function Update ()
+{
+	
+	timer -= Time.deltaTime;
+	rigidbody2D.velocity.y = speed;
+	if(timer <= 0)
+	
+	{
+		
+		speed = -speed;
+		timer = timeset;
+		
+	}
+};
