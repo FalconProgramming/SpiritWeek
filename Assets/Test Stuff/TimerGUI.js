48641374cade8fc9ff143683375da6bfff4e09da
Spinark167 <jhandrews1@gmail.com>
2014-11-30 19:21:02 -0500

    Fixed stuff and moved stuff.
    Moved the timer and movement scripts to the test scene and deleted a
    needless part of the timerGUI that was causing a warning message.

--- Assets/Test Stuff/TimerGUI.js
+++ Assets/Test Stuff/TimerGUI.js
@@ -7,8 +7,5 @@ function Update ()
 
 function OnGUI()
 {
-	if(1 == 1)
-	{
-		GUI.Box (Rect (20,10,60,25), GUIContent (timer.ToString("0")));
-	};
+	GUI.Box (Rect (20,10,60,25), GUIContent (timer.ToString("0")));
 };
