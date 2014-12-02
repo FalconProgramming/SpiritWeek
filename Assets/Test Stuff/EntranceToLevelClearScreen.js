e7821fb1e291d342043c29c2526816933e2c8e20
Edmond1 <edmonyx@yahoo.com>
2014-12-02 11:33:52 -0500

    Minor change to first level

---
+++ Assets/Test Stuff/EntranceToLevelClearScreen.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var Player : Rigidbody2D;
+
+function OnTriggerEnter2D (collision : Collider2D) {
+	if (collision.tag == "Player") {
+	
+		Application.LoadLevel ("Level Clear Screen");
+	}
+}
