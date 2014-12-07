34f22b833208287acc23afcc8ef6df01d0b04e74
Edmond1 <edmonyx@yahoo.com>
2014-12-06 19:15:12 -0500

    Changed font for Inventory UI

--- Assets/Test Stuff/InventoryUI.js
+++ Assets/Test Stuff/InventoryUI.js
@@ -2,9 +2,12 @@
 
 var pencil : Texture2D;
 var essay : Texture2D;
+var font : Font;
 
 function OnGUI () {
 if (Bookbag.bookbag) {
+
+	GUI.skin.font = font;
 
     GUI.Box (Rect (0,Screen.height - 50,100,50), GUIContent (InventoryManager.pencil.ToString(), pencil));
     
