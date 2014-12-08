438c24a3e3cc877b9b6451d980a1669109b5c9f8
Edmond1 <edmonyx@yahoo.com>
2014-12-08 18:52:03 -0500

    Finalized level transitions and basically the game

--- Assets/Test Stuff/EntranceToLevelClearScreen.js
+++ Assets/Test Stuff/EntranceToLevelClearScreen.js
@@ -4,7 +4,19 @@ var Player : Rigidbody2D;
 
 function OnTriggerEnter2D (collision : Collider2D) {
 	if (collision.tag == "Player") {
-		LevelSelect.lastLevelPlayed += 1;
-		Application.LoadLevel (LevelSelect.lastLevelPlayed);
+		if (Application.loadedLevelName == "College Day")
+	Application.LoadLevel ("Level Clear Screen for College Day");
+	
+	if (Application.loadedLevelName == "costume day")
+	Application.LoadLevel ("Level Clear Screen for Costume Day");
+	
+	if (Application.loadedLevelName == "red and blue day")
+	Application.LoadLevel ("Level Clear Screen for Red and Blue Day");
+	
+	if (Application.loadedLevelName == "throw back day")
+	Application.LoadLevel ("Level Clear Screen for Throw Back Day");
+	
+	if (Application.loadedLevelName == "NEON DAY")
+	Application.LoadLevel ("Level Clear Screen for Neon Day");
 	}
 }
